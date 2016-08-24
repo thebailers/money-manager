b2dd9a14f10d57408c3568a03e3c220a8407ae65
Simon <simon@pelli.co.uk>
2016-08-24 23:08:32 +0100

    Adds Jest & test for sumObjValues utility func

---
+++ __tests__/sumObjectValues-test.js
@@ -0,0 +1,30 @@
+import sumObjectValues from '../client/utils/sumObjectValues';
+
+const obj = [
+	{
+		"id": 0,
+		"item": "Tesla Model S",
+		"amount": 85000
+	},
+	{
+		"id": 1,
+		"item": "iPhone 6S",
+		"amount": 600
+	},
+	{
+		"id": 2,
+		"item": "MacBook Pro",
+		"amount": 1700
+	}
+];
+
+const identifier = "amount";
+
+
+jest.unmock('../client/utils/sumObjectValues'); // unmock to use the actual implementation of sumObjectValues
+
+describe('sumObjectValues', () => {
+	it('takes an array of objects, each with amount values, & sums the values', () => {
+		expect(sumObjectValues(obj, identifier)).toBe(87300);
+	});
+});
