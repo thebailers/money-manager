b56d588e1ad3acf5146789a96c9cb75d17fce441
Simon <simon@pelli.co.uk>
2016-09-05 19:11:35 +0100

    Replaces testing suite with Jest v15

---
+++ __tests__/components/Total.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Total from '../../client/components/Total';
+
+it('displays the label with the correct text', () => {
+	const type = "Transactions";
+	const component = TestUtils.renderIntoDocument(
+		<Total value="100" type={type} />
+	);
+	const spanLabel = TestUtils.findRenderedDOMComponentWithClass(
+		component,
+		'label'
+	);
+	expect(spanLabel.textContent).toEqual(type);
+});
+
+
+it('outputs the correctly formatted value', () => {
+	const component = TestUtils.renderIntoDocument(
+		<Total value="100" type="Transactions" />
+	);
+	const spanValue = TestUtils.findRenderedDOMComponentWithClass(
+		component,
+		'value'
+	);
+	expect(spanValue.textContent).toEqual('£100');
+});
