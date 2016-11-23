f61e3ca4a328ba856fabc925eb588bb4d894bd14
Simon Bailey <simon@pelli.co.uk>
2016-11-23 13:16:36 +0000

    Adds error handling to the api

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var path = require('path')
 var express = require('express')
 var app = express()
 var api = require('./api/')
+var err = require('./middleware/err')
 
 require('./middleware/appMiddleware')(app)
 
@@ -10,5 +11,7 @@ app.use('/api', api)
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+app.use(err)
 
 module.exports = app
