f77cc4ebc2a2f946e77d2fa6c21311ec9f57b110
Simon Bailey <simon@pelli.co.uk>
2017-01-25 23:07:32 +0000

    Remove unnecessary defaultState from createStore

--- client/store.js
+++ client/store.js
@@ -3,17 +3,8 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
 import rootReducer from './reducers/index'
-import transactionsReducer from './reducers/transactions'
-import expenditureReducer from './reducers/expenditure'
-import incomeReducer from './reducers/income'
 
-const defaultState = {
-  transactions: transactionsReducer,
-  expenditure: expenditureReducer,
-  income: incomeReducer
-}
-
-const store = createStore(rootReducer, defaultState)
+const store = createStore(rootReducer)
 export const history = syncHistoryWithStore(browserHistory, store)
 
 export default store
