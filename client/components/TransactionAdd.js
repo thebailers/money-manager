5cd68ead02dac49ca1b7d911e59e8ba913ec3313
Simon Bailey <simon@pelli.co.uk>
2017-02-02 23:31:51 +0000

    Remove redux form, implement react-dates for transaction add

--- client/components/TransactionAdd.js
+++ client/components/TransactionAdd.js
@@ -1,20 +1,76 @@
 import React, { Component, PropTypes } from 'react'
-import { reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { addTransaction } from '../actions/actionCreators'
+import { SingleDatePicker } from 'react-dates'
+import classnames from 'classnames'
+import moment from 'moment'
 
 class TransactionAdd extends Component {
 
   constructor () {
     super()
-    this.onSubmit = this.onSubmit.bind(this)
+    this.state = {
+      name: '',
+      date: moment(new Date()),
+      focused: false,
+      amount: '',
+      errors: {},
+      loading: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.onDateChange = this.onDateChange.bind(this)
+    this.onFocusChange = this.onFocusChange.bind(this)
   }
 
-  onSubmit (props) {
+  handleSubmit (e) {
+    e.preventDefault()
+    /*
     this.props.addTransaction(props)
       .then(() => {
         this.context.router.push('/')
       })
+    */
+
+    // valudation
+    let errors = {}
+    if (this.state.name === '') errors.name = 'Please enter a name'
+    if (this.state.date === '') errors.date = 'Please enter a date'
+    if (this.state.amount === '') errors.amount = 'Please enter a value'
+
+    this.setState({ errors })
+
+    const isValid = Object.keys(errors).length === 0
+
+    if (isValid) {
+      const { name, date, amount } = this.state
+      this.setState({ loading: true })
+      this.props.addTransaction({ name, date, amount })
+        .then(() => {
+          this.context.router.push('/')
+        })
+    }
+  }
+
+  handleChange (e) {
+    if (!this.state.errors[e.target.name]) {
+      let errors = Object.assign({}, this.state.errors)
+      delete errors[e.target.name]
+      this.setState({
+        [e.target.name]: e.target.value,
+        errors
+      })
+    }
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onDateChange (e) {
+    this.setState({ date: moment(e._d) })
+  }
+
+  onFocusChange () {
+    this.setState({ focused: !this.state.focused })
   }
 
   static contextTypes = {
@@ -22,23 +78,34 @@ class TransactionAdd extends Component {
   }
 
   render () {
-    const { fields: { name, amount, date }, handleSubmit } = this.props
-
     return (
-      <section>
+      <section className={classnames({loading: this.state.loading})}>
         <h2>Add Transaction <Link className="actionlink" to="/">Go back</Link></h2>
-        <form onSubmit={handleSubmit(this.onSubmit)}>
-          <label>Name</label>
-          <input type="text" {...name} />
-          <div className="text-help">{name.touched ? name.error : ''}</div>
+        <form onSubmit={this.handleSubmit}>
 
-          <label>Date</label>
-          <input type="date" {...date} />
-          <div className="text-help">{date.touched ? date.error : ''}</div>
+          <div className={classnames('field', { error: !!this.state.errors.name })}>
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+            <span className="text-help">{this.state.errors.name}</span>
+          </div>
 
-          <label>Amount</label>
-          <input type="text" {...amount} />
-          <div className="text-help">{amount.touched ? amount.error : ''}</div>
+          <div className={classnames('field', { error: !!this.state.errors.date })}>
+            <label htmlFor="date">Date</label>
+            <SingleDatePicker
+              id="date"
+              date={this.state.date}
+              focused={this.state.focused}
+              onDateChange={this.onDateChange}
+              onFocusChange={this.onFocusChange}
+            />
+            <span className="text-help">{this.state.errors.date}</span>
+          </div>
+
+          <div className={classnames('field', { error: !!this.state.errors.amount })}>
+            <label htmlFor="amount">Amount</label>
+            <input id="amount" name="amount" type="text" value={this.state.amount} onChange={this.handleChange} />
+            <span className="text-help">{this.state.errors.amount}</span>
+          </div>
 
           <button type="submit">Add</button>
         </form>
@@ -48,31 +115,7 @@ class TransactionAdd extends Component {
 }
 
 TransactionAdd.propTypes = {
-  addTransaction: React.PropTypes.func.isRequired,
-  fields: React.PropTypes.object.isRequired,
-  handleSubmit: React.PropTypes.func.isRequired
+  addTransaction: React.PropTypes.func.isRequired
 }
 
-function validate (values) {
-  const errors = {}
-
-  if (!values.name) {
-    errors.name = 'Please briefly describe the transaction.'
-  }
-
-  if (!values.date) {
-    errors.date = 'Please enter a date for your transaction.'
-  }
-
-  if (!values.amount) {
-    errors.amount = 'What was the value of the transaction?'
-  }
-
-  return errors
-}
-
-export default reduxForm({
-  form: 'AddTransaction',
-  fields: ['name', 'date', 'amount'],
-  validate
-}, null, { addTransaction })(TransactionAdd)
+export default connect(null, { addTransaction })(TransactionAdd)
