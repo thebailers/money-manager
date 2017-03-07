358effbf9adf1e4279e2e83a78b0f42a913f7bfa
Simon Bailey <simon@pelli.co.uk>
2017-03-07 13:31:31 +0000

    Conditionally renders guest/logged in header navigation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Header extends Component {
+class Header extends Component {
   render () {
+    const { isAuthenticated, user } = this.props.auth
+
+    const userLinks = (
+      <ul className='navigation'>
+        <li>Hello {user.username}</li>
+        <li><a href='#'>Accounts</a></li>
+        <li><a href='/login'>Log out</a></li>
+      </ul>
+    )
+
+    const guestLinks = (
+      <ul className='navigation'>
+        <li><a href='/login'>Log in</a></li>
+        <li><a className='signup button green' href='#'>Sign up</a></li>
+      </ul>
+    )
+
     return (
       <header>
         <div className='logo'>My £oney Manager</div>
-        <ul className='navigation'>
-          <li><a href='#'>Accounts</a></li>
-          <li><a href='/login'>Log in</a></li>
-          <li><a className='signup button green' href='#'>Sign up</a></li>
-        </ul>
+        {isAuthenticated ? userLinks : guestLinks}
       </header>
     )
   }
 }
+
+Header.propTypes = {
+  auth: React.PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(Header)
