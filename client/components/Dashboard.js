3bafd5b22fb4f7ad5f6665beda29d1bf30234d14
Simon Bailey <simon@pelli.co.uk>
2017-03-13 22:24:30 +0000

    Move dashboard transactions into child Component

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -2,25 +2,12 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actionCreators from '../actions/actionCreators'
-import { Link } from 'react-router'
-import Transaction from './Transaction'
+import Transactions from './Transactions'
 import Total from './Total'
 import Remaining from './Remaining'
 import sumObjectValues from '../utils/sumObjectValues'
-import _ from 'ramda'
-import diff from '../utils/diff'
 
-class Transactions extends Component {
-
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      orderBy: 'date-desc'
-    }
-
-    this.orderBy = this.orderBy.bind(this)
-  }
+class Dashboard extends Component {
 
   componentWillMount () {
     if (this.props.isAuthenticated) {
@@ -32,31 +19,6 @@ class Transactions extends Component {
 
   static contextTypes = {
     router: PropTypes.object
-  }
-
-  orderBy (e) {
-    let key = e.target.getAttribute('data-orderby')
-    const clickedSplitKey = _.split('-', key)
-    const stateSplitKey = _.split('-', this.state.orderBy)
-
-    // If the data is already ordered - reverse the ordering
-    if (_.split('-', this.state.orderBy)[0] === clickedSplitKey[0]) {
-      let keyVal = clickedSplitKey[0]
-      key = (stateSplitKey[1] === 'asc') ? `${keyVal}-desc` : `${keyVal}-asc`
-      this.setState({ orderBy: key })
-      return
-    }
-
-    this.setState({ orderBy: key })
-  }
-
-  orderData (data) {
-    // Specify how to sort based on the appended orderBy flag in this.state.orderBy
-    if (_.split('-', this.state.orderBy)[1] === 'asc') {
-      return _.sort(diff(_.split('-', this.state.orderBy)[0]), data)
-    }
-
-    return _.reverse(_.sort(diff(_.split('-', this.state.orderBy)[0]), data))
   }
 
   render () {
@@ -76,24 +38,7 @@ class Transactions extends Component {
 
     return (
       <section>
-        <h2>Transactions <Link className='actionlink' to='/transactions/add'>Add</Link></h2>
-        <table className='financials -transactions'>
-          <thead>
-            <tr>
-              <th className={(_.split('-', this.state.orderBy)[0] === 'name') ? 'activefilter' : ''} onClick={this.orderBy} data-orderby='name-asc'>Name</th>
-              <th className={((_.split('-', this.state.orderBy)[0] === 'date') ? 'activefilter' : '')} onClick={this.orderBy} data-orderby='date-desc'>Date</th>
-              <th className={(_.split('-', this.state.orderBy)[0] === 'amount') ? 'activefilter' : ''} onClick={this.orderBy} data-orderby='amount-asc'>Amount</th>
-              <th className='actions nobor'>&nbsp;</th>
-              <th className='actions nobor'>&nbsp;</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.orderData(transactions).map(
-              (transaction, i) =>
-                <Transaction {...this.props} key={i} transaction={transaction} />
-            )}
-          </tbody>
-        </table>
+        <Transactions transactions={transactions} />
 
         <section className='sumtotal'>
           <Total value={transactionsTotal} type='Transactions' />
@@ -114,7 +59,7 @@ class Transactions extends Component {
 }
 const { func, array, bool } = React.PropTypes
 
-Transactions.propTypes = {
+Dashboard.propTypes = {
   fetchExpenditure: func.isRequired,
   fetchIncome: func.isRequired,
   transactions: array,
@@ -135,4 +80,4 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actionCreators, dispatch)
 
-export default connect(mapStateToProps, mapDispatchToProps)(Transactions)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
---
+++ client/components/Transactions.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import Transaction from './Transaction'
+import _ from 'ramda'
+import diff from '../utils/diff'
+
+class Transactions extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      orderBy: 'date-desc'
+    }
+
+    this.orderBy = this.orderBy.bind(this)
+  }
+
+  orderBy (e) {
+    let key = e.target.getAttribute('data-orderby')
+    const clickedSplitKey = _.split('-', key)
+    const stateSplitKey = _.split('-', this.state.orderBy)
+
+    // If the data is already ordered - reverse the ordering
+    if (_.split('-', this.state.orderBy)[0] === clickedSplitKey[0]) {
+      let keyVal = clickedSplitKey[0]
+      key = (stateSplitKey[1] === 'asc') ? `${keyVal}-desc` : `${keyVal}-asc`
+      this.setState({ orderBy: key })
+      return
+    }
+
+    this.setState({ orderBy: key })
+  }
+
+  orderData (data) {
+    // Specify how to sort based on the appended orderBy flag in this.state.orderBy
+    if (_.split('-', this.state.orderBy)[1] === 'asc') {
+      return _.sort(diff(_.split('-', this.state.orderBy)[0]), data)
+    }
+
+    return _.reverse(_.sort(diff(_.split('-', this.state.orderBy)[0]), data))
+  }
+
+  render () {
+    const { transactions } = this.props
+    return (
+      <section className='transactions'>
+        <h2>Transactions <Link className='actionlink' to='/transactions/add'>Add</Link></h2>
+        <table className='financials -transactions'>
+          <thead>
+            <tr>
+              <th className={(_.split('-', this.state.orderBy)[0] === 'name') ? 'activefilter' : ''} onClick={this.orderBy} data-orderby='name-asc'>Name</th>
+              <th className={((_.split('-', this.state.orderBy)[0] === 'date') ? 'activefilter' : '')} onClick={this.orderBy} data-orderby='date-desc'>Date</th>
+              <th className={(_.split('-', this.state.orderBy)[0] === 'amount') ? 'activefilter' : ''} onClick={this.orderBy} data-orderby='amount-asc'>Amount</th>
+              <th className='actions nobor'>&nbsp;</th>
+              <th className='actions nobor'>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.orderData(transactions).map(
+              (transaction, i) =>
+                <Transaction {...this.props} key={i} transaction={transaction} />
+            )}
+          </tbody>
+        </table>
+      </section>
+    )
+  }
+}
+
+Transactions.propTypes = {
+  transactions: React.PropTypes.array
+}
+
+export default Transactions
