6b974db3f3adaf305d2d9f4c54ca911c158d7835
Simon Bailey <simon@pelli.co.uk>
2017-03-16 18:03:02 +0000

    test FETCH_TRANSACTION & export initial state for testing

--- __tests__/reducers/transactions_reducer.test.js
+++ __tests__/reducers/transactions_reducer.test.js
@@ -1,12 +1,72 @@
 import reducer from '../../client/reducers/transactions'
 import * as types from '../../client/actions/actionCreators'
+import { INITIAL_STATE } from '../../client/reducers/transactions'
+
+const transactions = [
+  {
+    "_id": "588900efdf9d3e0905a2d604",
+    "amount": 4.50,
+    "name": "Cashew Nuts",
+    "__v": 0,
+    "date": "2017-01-25T00:00:00.000Z",
+    "user": "58c2a33cc6cd5a5d15a8fc0c"
+  },
+  {
+    "_id": "58890108df9d3e0905a2d605",
+    "amount": 6.25,
+    "name": "Monmouth Coffee",
+    "__v": 0,
+    "date": "2017-01-25T00:00:00.000Z",
+    "user": "58c2a33cc6cd5a5d15a8fc0c"
+  },
+  {
+    "_id": "588b67fbada9d2040bee4ef4",
+    "amount": 399,
+    "name": "Flight to Copenhagen",
+    "__v": 0,
+    "date": "2017-01-27T00:00:00.000Z",
+    "user": "58c2a33cc6cd5a5d15a8fc0c"
+  }
+]
 
 describe('transactions reducer', () => {
   it('should return the initial state', () => {
     expect(
       reducer(undefined, {})
     ).toEqual(
-      { all: [], transaction: {} }
+      INITIAL_STATE
+    )
+  })
+
+  it('should handle FETCH_TRANSACTIONS', () => {
+    expect(
+      reducer(
+        INITIAL_STATE, {
+          type: types.FETCH_TRANSACTION,
+          payload: {
+            data: {
+              "_id": "58890108df9d3e0905a2d605",
+              "amount": 6.25,
+              "name": "Monmouth Coffee",
+              "__v": 0,
+              "date": "2017-01-25T00:00:00.000Z",
+              "user": "58c2a33cc6cd5a5d15a8fc0c"
+            }
+          }
+        }
+      )
+    ).toEqual(
+      {
+        all: [],
+        transaction: {
+          "_id": "58890108df9d3e0905a2d605",
+          "amount": 6.25,
+          "name": "Monmouth Coffee",
+          "__v": 0,
+          "date": "2017-01-25T00:00:00.000Z",
+          "user": "58c2a33cc6cd5a5d15a8fc0c"
+        }
+      }
     )
   })
 })
