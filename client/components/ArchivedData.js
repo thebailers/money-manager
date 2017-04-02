73c5bbaacf6cea4fae2c4a850a550e0a3446d735
Simon Bailey <simon@pelli.co.uk>
2017-04-02 21:50:33 +0100

    Call transactions action for selected archive date

--- client/components/ArchivedData.js
+++ client/components/ArchivedData.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import moment from 'moment'
+import monthNames from '../utils/monthNames'
+import { fetchTransactions } from '../actions/actionCreators'
+import Transactions from './Transactions'
 
 class ArchivedData extends Component {
+  componentDidMount () {
+    const { month, year } = this.props.params
+
+    const monthToInt = monthNames.indexOf(month)
+    const daysInMonth = moment(`${year}-${monthToInt}`, 'YYYY-MM').daysInMonth()
+
+    const start = new Date(year, month, 1)
+    const end = new Date(year, month, daysInMonth)
+
+    this.props.fetchTransactions(start, end)
+  }
   render () {
+    const { transactions } = this.props
+
+    if (!transactions) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )
+    }
+
     return (
-      <div>
-        Placeholder: time to add in the data for {this.props.params.month} {this.props.params.year}
-      </div>
+      <section>
+        <h2>Transactions for {this.props.params.month} {this.props.params.year}</h2>
+        <Transactions transactions={transactions} />
+      </section>
     )
   }
 }
 
+const { object, func, array } = React.PropTypes
+
 ArchivedData.propTypes = {
-  params: React.PropTypes.object.isRequired
+  params: object.isRequired,
+  fetchTransactions: func.isRequired,
+  transactions: array
 }
 
-export default ArchivedData
+const mapStateToProps = (state) => {
+  return {
+    transactions: state.transactions.all
+  }
+}
+
+export default connect(mapStateToProps, { fetchTransactions })(ArchivedData)
